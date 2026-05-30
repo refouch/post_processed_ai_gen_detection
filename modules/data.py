6140a9d9c35ca4c3a16ef3ee1adb936a82a2d3c2
refouch <remi.foucherand@ensae.fr>
2026-05-30 20:34:47 +0200

    feat: custom pytorch dataset/dataloader

--- modules/data.py
+++ modules/data.py
@@ -1,1 +1,36 @@
 """Data preprocesssing and DataLoader objects"""
+
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
+from PIL import Image
+import pandas as pd
+import os
+
+TRANSFORMS    = ["original", "transfer", "redigital"]
+REALFAKE_MAP  = {"real": 0, "ai": 1}
+TRANSFORM_MAP = {"original": 0, "transfer": 1, "redigital": 2}
+
+class RRDataset(Dataset):
+    """Adapting the pytorch dataset to take into input our custom csv pointing to each file,
+        as the normal ImageDataset won't work here (splits not in the same folder)"""
+
+    def __init__(self, wide_df, split, apply_transform=None):
+        sub = wide_df[wide_df.split == split]
+        rows = []
+        for _, row in sub.iterrows():
+            for transform in TRANSFORMS:
+                rows.append((row[transform], row["label"], transform))
+        self.samples = rows
+        self.transform = apply_transform  # CAUTION: different from the dataset "transform" column
+
+    def __len__(self):
+        return len(self.samples)
+
+    def __getitem__(self, idx):
+        filepath, label, transform = self.samples[idx]
+        img = Image.open(filepath).convert("RGB")
+        if self.transform:
+            img = self.transform(img)
+        y_label     = REALFAKE_MAP[label]
+        y_transform = TRANSFORM_MAP[transform]
+        return img, y_label, y_transform
--- modules/utils.py
+++ modules/utils.py
@@ -1,1 +1,19 @@
 """Useful functions"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def show_sample(img_tensor, label):
+    """Code borrowed from tutorial n°2"""
+    
+    img = img_tensor.numpy().transpose((1, 2, 0))
+
+    mean = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229, 0.224, 0.225])
+    img = std * img + mean
+    img = np.clip(img, 0, 1)
+
+    plt.imshow(img)
+    plt.title(f"Class: {label}, Shape: ({img_tensor.shape[0]},{img_tensor.shape[1]},{img_tensor.shape[2]})")
+    plt.axis('off')
+    plt.show()
